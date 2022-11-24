3ceb00d6a312b0f99fc4ce44d30fcfe62cd7d0c8
prasad77ghosh <prasadghosh997@gmail.com>
2022-11-24 21:53:55 +0530

    deployed on cyclic

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,6 @@
 const cookieParser = require("cookie-parser");
 const express = require("express");
 const cors = require("cors");
-const dotenv = require("dotenv");
 const path = require("path");
 const errorMiddleware = require("./middlewares/error");
 const bodyParser = require("body-parser");
