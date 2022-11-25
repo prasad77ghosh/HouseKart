c4fb8336fc031c1d084ce0956816b1de4a16eb3b
prasad77ghosh <prasadghosh997@gmail.com>
2022-11-25 09:57:06 +0530

    deployed on cyclic

--- frontend/src/http.js
+++ frontend/src/http.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://192.168.29.21:5000/api/v1",
+  baseURL: "http://localhost:5000/api/v1",
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
