c344fd7c8c275833c7d70dc8c7a28d2f5580d7e2
prasad77ghosh <prasadghosh997@gmail.com>
2022-11-25 11:11:50 +0530

    deployed on cyclic

--- backend/app.js
+++ backend/app.js
@@ -6,14 +6,23 @@ const errorMiddleware = require("./middlewares/error");
 const bodyParser = require("body-parser");
 const fileupload = require("express-fileupload");
 const app = express();
-
+const corsOptions = {
+  origin: "http://localhost:3000",
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
 //config
 
 if (process.env.NODE_ENV !== "production") {
   require("dotenv").config({ path: "backend/config/config.env" });
 }
 
-app.use(cors());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  next();
+});
+
+app.use(cors(corsOptions));
 app.use(express.json({ limit: "20mb" }));
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
@@ -24,6 +33,7 @@ const productsRoutes = require("./routes/productRoute");
 const usersRoutes = require("./routes/userRoutes");
 const ordersRoutes = require("./routes/orderRoutes");
 const paymentRoute = require("./routes/paymentRoute");
+
 
 app.use("/api/v1", productsRoutes);
 app.use("/api/v1", usersRoutes);
--- frontend/src/http.js
+++ frontend/src/http.js
@@ -2,6 +2,7 @@ import axios from "axios";
 
 export default axios.create({
   baseURL: "http://localhost:5000/api/v1",
+  withCredentials: true,
   headers: {
     "Content-Type": "application/json",
     Accept: "application/json",
