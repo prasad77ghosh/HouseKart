48fb2d3c42e6af0077295d03573af7c31b100383
prasad77ghosh <prasadghosh997@gmail.com>
2022-11-25 11:32:47 +0530

    deployed on cyclic

--- backend/app.js
+++ backend/app.js
@@ -18,7 +18,11 @@ if (process.env.NODE_ENV !== "production") {
 }
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
   next();
 });
 
