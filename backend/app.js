bfbe2422bdb13cc2e789d16f6bd0af536370e518
prasad77ghosh <prasadghosh997@gmail.com>
2022-11-25 13:41:50 +0530

    deployed on cyclic

--- backend/app.js
+++ backend/app.js
@@ -17,7 +17,7 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use(cors({ credentials: true}));
+app.use(cors());
 app.use(express.json({ limit: "20mb" }));
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
