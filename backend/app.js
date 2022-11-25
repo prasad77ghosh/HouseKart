fb08fa8ba064d6aee383ede7ecb6489d42d58d58
prasad77ghosh <prasadghosh997@gmail.com>
2022-11-25 13:39:08 +0530

    deployed on cyclic

--- backend/app.js
+++ backend/app.js
@@ -13,7 +13,10 @@ if (process.env.NODE_ENV !== "production") {
 }
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Origin",
+    "https://busy-jade-seagull-tutu.cyclic.app"
+  );
   next();
 });
 
