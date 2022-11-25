75e7dc4f0f4c40ceeb6c8dfbbc66643ae44552e2
prasad77ghosh <prasadghosh997@gmail.com>
2022-11-25 13:59:11 +0530

    deployed on cyclic

--- frontend/src/http.js
+++ frontend/src/http.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:5000/api/v1",
+  baseURL: "https://busy-jade-seagull-tutu.cyclic.app/api/v1",
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
