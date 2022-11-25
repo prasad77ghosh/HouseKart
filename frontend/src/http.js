294d6e70a99b2edc44fd2219b84c7956ffab8f70
prasad77ghosh <prasadghosh997@gmail.com>
2022-11-25 07:35:39 +0530

    deployed on cyclic

--- frontend/src/http.js
+++ frontend/src/http.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://fine-tan-bass-sari.cyclic.app/api/v1",
+  baseURL: "http://localhost:5000/api/v1",
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
