1580cbc24483fc07bf46d5eefc0dbe5b04b6a7ff
prasad77ghosh <prasadghosh997@gmail.com>
2022-11-25 13:35:28 +0530

    deployed on cyclic

--- backend/app.js
+++ backend/app.js
@@ -12,9 +12,17 @@ if (process.env.NODE_ENV !== "production") {
   require("dotenv").config({ path: "backend/config/config.env" });
 }
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+    return res.status(200).json({});
+  }
+  res.header("Access-Control-Allow-Credentials", true);
+  next();
+});
 
-
-app.use(cors({ credentials: true}));
+app.use(cors({ credentials: true }));
 app.use(express.json({ limit: "20mb" }));
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
@@ -26,7 +34,6 @@ const usersRoutes = require("./routes/userRoutes");
 const ordersRoutes = require("./routes/orderRoutes");
 const paymentRoute = require("./routes/paymentRoute");
 
-
 app.use("/api/v1", productsRoutes);
 app.use("/api/v1", usersRoutes);
 app.use("/api/v1", ordersRoutes);
@@ -34,7 +41,7 @@ app.use("/api/v1", paymentRoute);
 
 app.use(express.static(path.join(__dirname, "../frontend/build")));
 
-app.get("*", (req, res) => {
+app.get("/*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
 });
 
